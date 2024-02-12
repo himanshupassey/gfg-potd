d8b5095c70481c1664b6d37de901e849867fb6e8
Himanshu Passey <146419971+himanshupassey@users.noreply.github.com>
2024-02-12 17:23:46 +0530

    Create 12-Feb-2024.cpp

---
+++ 2024/FEB/12-Feb-2024.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long sequence(int n){
+        long long mod = 1e9 + 7;
+        long long out = 0, c = 1;
+        for(int i = 1; i <= n; ++i){
+            long long temp = 1;
+            for(int j = 0; j < i ; ++j){
+                temp *= c++;
+                temp %= mod;
+            }
+            out = (out + temp) % mod;
+        }
+        return out;
+    }
+};
