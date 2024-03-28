d91fb6af47628ddd3b63556ec270bd511b15813b
Himanshu Passey <146419971+himanshupassey@users.noreply.github.com>
2024-03-28 13:20:45 +0530

    Create 26-MAR-2024.cpp

---
+++ 2024/MAR/26-MAR-2024.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(int ii, string &s, int n) {
+        if (ii >= n)
+            return true;
+
+        int a = 0, b = 0;
+        for (int i = ii; i < n; ++i) {
+            a = a * 10 + (s[i] - '0');
+            b = 0;
+            for (int j = i + 1; j < n; ++j) {
+                b = b * 10 + (s[j] - '0');
+                string c = to_string(a + b);
+                if (c.size() <= n - j - 1)
+                    if (c == s.substr(j + 1, c.size()))
+                        if (solve(j + c.size() + 1, s, n))
+                            return true;
+            }
+        }
+        return false;
+    }
+
+    bool isAdditiveSequence(string n) {
+        return solve(0, n, n.size());
+    }
+};
