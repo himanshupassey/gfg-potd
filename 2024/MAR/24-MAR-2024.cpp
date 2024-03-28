b3fe8144fc9068498f9ee6f26e9e2e4b3b09831d
Himanshu Passey <146419971+himanshupassey@users.noreply.github.com>
2024-03-28 13:21:19 +0530

    Create 24-MAR-2024.cpp

---
+++ 2024/MAR/24-MAR-2024.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x)
+    {
+        stack<int>temp;
+        while (!st.empty())
+        {
+            temp.push(st.top());
+            st.pop();
+        }
+        st.push(x);
+        while (!temp.empty())
+        {
+            st.push(temp.top());
+            temp.pop();
+        }
+        return st;
+    }
+};
